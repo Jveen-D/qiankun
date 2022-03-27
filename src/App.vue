8022fff275b3782e1da8229a8146ffdd65c2fd06
杜文杰 <d85066965@icloud.com>
2022-03-27 17:35:21 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <!--
  * @Date: 2022-03-21 13:58:42
  * @LastEditors: dwj18066042960
- * @FilePath: \Test\src\App.vue
+ * @FilePath: \qiankun\src\App.vue
 -->
 <template>
-  <router-view></router-view>
+<div class="h-[40px] bg-blue-300 flex items-center px-4">
+    <!-- 子应用路由跳转 -->
+    <router-link class="text-white" to="/child/child1">child/child1</router-link>
+</div>
+<!-- 子应用 -->
+<div id="TestApp"></div>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-03-21 13:58:42
  * @LastEditors: dwj18066042960
- * @FilePath: \Test\src\main.js
+ * @FilePath: \qiankun\src\main.js
  */
 import { createApp } from "vue";
 import "./App.css";
@@ -10,3 +10,16 @@ import router from "./router";
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+
+
+import { registerMicroApps, start } from 'qiankun';
+registerMicroApps([
+  {
+    name: 'Test app', // app name registered
+    entry: { scripts: ['//localhost:3001'] },
+    container: '#TestApp',
+    activeRule: '/test',
+  }
+]);
+
+start();
