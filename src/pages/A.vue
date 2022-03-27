9ab9b6841bc3a5088550acc376b25ed8d92f2de1
杜文杰 <d85066965@icloud.com>
2022-03-27 16:50:14 +0800

    chore:模板重置

--- src/pages/A.vue
+++ src/pages/A.vue
@@ -1,17 +1,10 @@
 <!--
  * @Date: 2022-03-27 13:07:28
  * @LastEditors: dwj18066042960
- * @FilePath: \Test\src\pages\A.vue
+ * @FilePath: \qiankun\src\pages\A.vue
 -->
 <template>
-<div>This is A</div>
-<Test></Test>
-<Test2></Test2>
+<div>App</div>
 </template>
 <script lang="ts" setup>
-import Test from './components/Test.vue'
-import Test2 from './components/Test2.vue'
-window.onresize = ()=>{
-    console.log('A component resize')
-}
 </script>
